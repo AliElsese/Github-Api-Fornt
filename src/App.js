d5b2ffc248a9e57fd8015c4aedd5c4d4772f9cb9
eldeveloper90 <eldeveloper90@gmail.com>
2023-01-12 00:42:11 +0200

    set Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Main Components
+import Home from "./main-components/Home";
+import LocalRepos from "./main-components/LocalRepos";
+import Login from "./main-components/login-component/Login";
+import Signup from "./main-components/signup-component/Signup";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="signup" element={<Signup />}></Route>
+        <Route path="home/*" element={<Home />}></Route>
+        <Route path="localRepos" element={<LocalRepos />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/env.js
@@ -0,0 +1,5 @@
+const environment = {
+  API_URL: "http://localhost:8001",
+};
+
+export default environment;
